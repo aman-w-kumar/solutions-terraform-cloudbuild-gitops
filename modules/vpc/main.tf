2c8c3f5b4b9db4e1589f236d419503aad33e6ed3
cloud user <cloud_user_p_501400cb@linuxacademygclabs.com>
2020-07-30 09:52:29 -0400

    update vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,34 +1,15 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "2.0.0"
-
-  project_id   = "${var.project}"
-  network_name = "${var.env}"
-
-  subnets = [
-    {
-      subnet_name   = "${var.env}-subnet-01"
-      subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = "us-west1"
-    },
-  ]
-
-  secondary_ranges = {
-    "${var.env}-subnet-01" = []
+resource "google_compute_subnetwork" "ez-net-prv-pr-dev-128" {
+  name          = "ez-net-prv-pr-dev-128"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
   }
+}
+
+resource "google_compute_network" "custom-test" {
+  name                    = "ez-vpc-pr-dev-eu-w3"
+  auto_create_subnetworks = false
 }
