192a842316c91d89c4551047cb5b8e6b219b2e3f
cloud user <cloud_user_p_501400cb@linuxacademygclabs.com>
2020-07-30 09:54:36 -0400

    update vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "ez-net-prv-pr-dev-128" {
   name          = "ez-net-prv-pr-dev-128"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.custom-test.id
+  network       = "google_compute_network.custom-test.id"
   secondary_ip_range {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
